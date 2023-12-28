a974e99d5e9a32cace3d55bf0460afd83514fdeb
Shisha1536 <schischkoff2017@yandex.ru>
2023-12-28 14:05:35 +0300

    Модуль 2 дз_3

---
+++ Course_2/dz_3/index.js
@@ -0,0 +1,12 @@
+// Задание 1
+const name = `Джон`;
+const admin = name;
+alert(admin);
+
+// Задание 2
+//hello 1
+//hello name
+//hello Ivan
+
+//Задание 3
+// Строка, число, булево, null, undefined, object, Array, function;
