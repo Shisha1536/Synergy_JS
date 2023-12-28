63a66a121b90a6d0fec1323db7ccbdef36494549
Shisha1536 <schischkoff2017@yandex.ru>
2023-12-28 14:23:48 +0300

    Модуль 2 дз 4

---
+++ Course_2/dz_4/index.js
@@ -0,0 +1,20 @@
+//Задание 1
+const srting = "5";
+const string2 = '3';
+
+let a = Number(srting);
+let b = srting + string2;
+let b2 = Number(srting) + Number(string2);
+let c = srting - string2;
+let d = srting * string2;
+let e = srting / string2;
+let f = srting % string2;
+let g = srting ** string2;
+
+//Задание 2
+console.log(srting == string2);
+console.log(srting != string2);
+console.log(srting > string2);
+console.log(srting >= string2);
+console.log(srting < string2);
+console.log(srting <= string2);
