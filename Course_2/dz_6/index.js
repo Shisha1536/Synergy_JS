f4ef186151edb7fa6c1cffbded625f624e589fe4
Shisha1536 <135944742+Shisha1536@users.noreply.github.com>
2024-01-03 13:31:27 +0300

    M_2 dz_6/2

--- Course_2/dz_6/index.js
+++ Course_2/dz_6/index.js
@@ -2,8 +2,8 @@ const a = 5;
 const b = 7;
 
 function squareOfSum(x,y) {
-    console.log ((x+y)**2);
-    console.log (Math.pow(x+y, 2));
-    console.log (x**2+2*x*y+y**2);
+    console.log (`squareOfSum ${x**2+2*x*y+y**2}`);
 }
 squareOfSum(a,b);
+ let squareOfSum_2 = (x, y) => console.log (`squareOfSum_2 ${Math.pow(x+y, 2)}`);
+ squareOfSum_2(a, b);
